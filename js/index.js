d49e3a308ea077c0b954ccae2f37ec99b5eca46d
Nazarena Arce <nazarena.arce8@gmail.com>
2022-06-04 20:10:50 -0300

    cambios en index

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+if ((nombre != "") && (apellido != "")) {
+    alert ("Bienvenido/a Simmer "+nombre+ " "+apellido+". Presiona 'Aceptar' para navegar en la web")
+}
+else {
+    alert ("!ERROR! Ingrese sus datos para navegar en la web")
+}
